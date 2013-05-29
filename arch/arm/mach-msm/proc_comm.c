4bcac7f3f870f72e02b3e40925a905eb591be1e4
Roman Yepishev <roman.yepishev@gmail.com>
2013-05-29 08:31:12 +0000

    Acer changes for smemlog.
    I haven't seen that yet but it may be useful.

--- arch/arm/mach-msm/proc_comm.c
+++ arch/arm/mach-msm/proc_comm.c
@@ -26,6 +26,9 @@
 #include "proc_comm.h"
 #include "smd_private.h"
 
+#ifdef CONFIG_MACH_ACER_A1
+#include <mach/smem_log.h>
+#endif
 #if defined(CONFIG_ARCH_MSM7X30)
 #define MSM_TRIG_A2M_PC_INT (writel(1 << 6, MSM_GCC_BASE + 0x8))
 #elif defined(CONFIG_ARCH_MSM8X60)
@@ -34,6 +37,9 @@
 #define MSM_TRIG_A2M_PC_INT (writel(1, MSM_CSR_BASE + 0x400 + (6) * 4))
 #endif
 
+#ifdef CONFIG_MACH_ACER_A1
+extern int smemlog_initialized;
+#endif
 static inline void notify_other_proc_comm(void)
 {
 	MSM_TRIG_A2M_PC_INT;
@@ -61,7 +67,35 @@ static DEFINE_SPINLOCK(proc_comm_lock);
  */
 static int proc_comm_wait_for(unsigned addr, unsigned value)
 {
+#ifdef CONFIG_MACH_ACER_A1
+	unsigned int count = 0;
+	unsigned base = (unsigned)MSM_SHARED_RAM_BASE;
+#endif
 	while (1) {
+#ifdef CONFIG_MACH_ACER_A1
+		count++;
+		if(count == 2000)
+		{
+			//leave a smem log before modem watchdog timeout happened
+			unsigned cmd = readl(base + APP_COMMAND);
+			smem_log_event(SMEM_LOG_PROC_ID_APPS | SMEM_LOG_EVENT_READ, cmd, 0, 0);
+		}
+		else if(count >= 200000)
+		{
+			unsigned cmd = readl(base + APP_COMMAND);
+			unsigned data1 = readl(base + APP_DATA1);
+			unsigned data2 = readl(base + APP_DATA2);
+			printk(KERN_INFO "ERROR : proc_comm no response, try again\n");
+			printk(KERN_INFO "cmd = 0x%x\n", cmd);
+			printk(KERN_INFO "data1 = 0x%x\n", data1);
+			printk(KERN_INFO "data2 = 0x%x\n", data2);
+			printk(KERN_INFO "MDM_STATUS = 0x%x\n", readl(base + MDM_STATUS));
+			dump_stack();
+
+			smem_log_event(SMEM_LOG_PROC_ID_APPS | ERR_ERROR_FATAL, cmd, data1, data2);
+			return 1;
+		}
+#endif
 		if (readl(addr) == value)
 			return 0;
 
--- kernel/panic.c
+++ kernel/panic.c
@@ -23,6 +23,12 @@
 #include <linux/nmi.h>
 #include <linux/dmi.h>
 
+#ifdef CONFIG_MACH_ACER_A1
+#include <asm/cacheflush.h>
+#include "../arch/arm/mach-msm/include/mach/shared_acer.h"
+#include "../arch/arm/mach-msm/proc_comm.h"
+#endif
+
 int panic_on_oops;
 static unsigned long tainted_mask;
 static int pause_on_oops;
@@ -61,6 +67,11 @@ NORET_TYPE void panic(const char * fmt, ...)
 	va_list args;
 	long i;
 
+#ifdef CONFIG_MACH_ACER_A1
+	acer_smem_proc_cmd_type proc_comm_type;
+	int ret = 0;
+#endif
+
 	/*
 	 * It's possible to come here directly from a panic-assertion and
 	 * not have preempt disabled. Some functions called from here want
@@ -92,6 +103,17 @@ NORET_TYPE void panic(const char * fmt, ...)
 	smp_send_stop();
 
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
+
+#ifdef CONFIG_MACH_ACER_A1
+	flush_cache_all();
+	//instruct modem side to enter ram dump mode
+	proc_comm_type = ACER_SMEM_PROC_CMD_OS_RAM_DUMP;
+	ret = msm_proc_comm(PCOM_CUSTOMER_CMD1, &proc_comm_type, 0);
+	if(ret == -1)
+		pr_err("Entering ram dump mode error\n");
+	else
+		pr_info("Enter ram dump mode\n");
+#endif
 
 	bust_spinlocks(0);
 
